efc90ce2a7ffa16b62017149ac1cc9f34e7e0243
hienle2703 <levutoanhien2703@gmail.com>
2023-08-05 14:52:53 +0700

    Add UI for Home Screen

--- src/screens/Home/HomeScreen.tsx
+++ src/screens/Home/HomeScreen.tsx
@@ -1,27 +1,84 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import Icon from 'react-native-vector-icons/MaterialIcons'
 import styles from './Styles/HomeScreenStyles'
+import LinearGradient from 'react-native-linear-gradient'
+import FastImage from 'react-native-fast-image'
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView>
-      <View style={styles.headerWrap}>
-        <View style={styles.headerPage}>
-          <View style={styles.avatarContainer}>
-            <Text>Avatar</Text>
+    <SafeAreaView style={styles.screenBase}>
+      <View style={styles.headerWrapBase}>
+        <LinearGradient
+          start={{ x: 0, y: 1 }}
+          end={{ x: 1, y: 0 }}
+          colors={['#000000', '#17B556']}
+          style={styles.headerWrap}
+        >
+          <View style={styles.headerPage}>
+            <View style={styles.avatarContainer}>
+              <FastImage
+                source={{
+                  uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGOIY_xfOWs3j5IVU6oSrWaID4BopWqYAV1hQ9pK4WO_uXc68fLuQfZWjF3epxPVpgEU8&usqp=CAU'
+                }}
+                style={styles.avatar}
+                resizeMode={FastImage.resizeMode.contain}
+              />
+            </View>
+            <Text style={styles.homeTitle}>Home</Text>
+            <TouchableOpacity style={styles.searchBox}>
+              <LinearGradient
+                start={{ x: 0, y: 1 }}
+                end={{ x: 1, y: 0 }}
+                colors={['#000000', '#17B556']}
+                style={styles.searchLinear}
+              >
+                <Icon name='search' color='white' size={20} />
+              </LinearGradient>
+            </TouchableOpacity>
           </View>
-          <Text style={styles.homeTitle}>Home</Text>
-          <View>
-            <Text>SearchBox</Text>
+          <View style={styles.headerRow}>
+            <TouchableOpacity style={styles.headerBoxContainer}>
+              <Icon
+                name='payments'
+                color='white'
+                style={styles.iconMenu}
+                size={20}
+              />
+              <Text style={styles.headerBoxText}>Create Income</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={[styles.headerBoxContainer, styles.expenseBox]}
+            >
+              <Icon
+                name='done'
+                color='white'
+                style={styles.iconMenu}
+                size={20}
+              />
+              <Text style={styles.headerBoxText}>Create Expense</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={[styles.headerBoxContainer, styles.budgetBox]}
+            >
+              <Icon
+                name='people'
+                color='white'
+                style={styles.iconMenu}
+                size={20}
+              />
+              <Text style={styles.headerBoxText}>{'Add\nBudget'}</Text>
+            </TouchableOpacity>
           </View>
-        </View>
-        <View style={styles.headerRow}>
-          <View style={styles.headerBoxContainer}>
-            <Icon name='payments' />
-            <Text style={styles.headerBoxText}>Create Income</Text>
-          </View>
+        </LinearGradient>
+      </View>
+      <View style={styles.body}>
+        <View style={styles.bodyHeader}>
+          <Text style={styles.bodyTitle}>Recent Transactions</Text>
+          <TouchableOpacity>
+            <Text style={styles.seeAllText}>See all</Text>
+          </TouchableOpacity>
         </View>
       </View>
     </SafeAreaView>
